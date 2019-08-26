e627560e01ba3308f40e729d2b1993a86b367f43
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-26 17:10:50 +0100

    Code example to generic repository

--- CoreApp.Data/Repositories/BaseRepository.cs
+++ CoreApp.Data/Repositories/BaseRepository.cs
@@ -12,5 +12,34 @@ namespace CoreApp.Data.Repositories
         {
             _context = context;
         }
+        
+        public IQueryable<TEntity> Get<TEntity>() where TEntity : class => _context.Set<TEntity>();
+        
+        public async Task<IEnumerable<TEntity>> GetAsync<TEntity>() where TEntity : class
+            => await _context.Set<TEntity>().ToListAsync();
+
+        public IQueryable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+            => _context.Set<TEntity>().Where(predicate);
+
+        public async Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+            => await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        
+        public TEntity GetObject<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+            => _context.Set<TEntity>().FirstOrDefault(predicate);
+
+        public async Task<TEntity> GetObjectAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class 
+            => await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+
+        public async Task<int> Add<TEntity>(TEntity entity)
+        {
+            _context.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Update<TEntity>(TEntity entity)
+        {
+            _context.Update(entity);
+            return await _context.SaveChangesAsync();
+        }
     }
 }
