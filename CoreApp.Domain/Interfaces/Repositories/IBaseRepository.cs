14cc7258e3d3ce14fb95dfadd1d339ebc91ba793
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-27 00:53:15 +0100

    Implementing base interface

--- CoreApp.Domain/Interfaces/Repositories/IBaseRepository.cs
+++ CoreApp.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -2,5 +2,16 @@
 {
     public interface IBaseRepository
     {
+        IQueryable<TEntity> Get<TEntity>();
+        Task<IEnumerable<TEntity>> GetAsync<TEntity>();
+
+        IQueryable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity GetObject<TEntity>(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetObjectAsync<TEntity>(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> Add<TEntity>(TEntity entity);
+        Task<int> Update<TEntity>(TEntity entity);
     }
 }
