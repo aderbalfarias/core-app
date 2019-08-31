95dd5751e3c42fbff1f64790651af594227a1670
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-31 11:51:10 +0100

    Adding test project

---
+++ CoreApp.UnitTest/DomainTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreApp.UnitTest
+{
+    [TestClass]
+    public class DomainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
