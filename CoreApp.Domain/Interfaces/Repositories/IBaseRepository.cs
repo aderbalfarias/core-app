50e2b1016e651d928c1c17953ca49a6a75f21cb0
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-06 01:11:56 +0100

    Fixing issues with repository

--- CoreApp.Domain/Interfaces/Repositories/IBaseRepository.cs
+++ CoreApp.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -8,16 +8,13 @@ namespace CoreApp.Domain.Interfaces.Repositories
 {
     public interface IBaseRepository
     {
-        IQueryable<TEntity> Get<TEntity>();
-        Task<IEnumerable<TEntity>> GetAsync<TEntity>();
-
-        IQueryable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> Get<TEntity>() where TEntity : class;
+        Task<IEnumerable<TEntity>> GetAsync<TEntity>() where TEntity : class;
+        IQueryable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
         IQueryable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> predicate, string include) where TEntity : class;
-        Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> predicate);
-
-        TEntity GetObject<TEntity>(Expression<Func<TEntity, bool>> predicate);
-        Task<TEntity> GetObjectAsync<TEntity>(Expression<Func<TEntity, bool>> predicate);
-
+        Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
+        TEntity GetObject<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
+        Task<TEntity> GetObjectAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
         Task<int> Add<TEntity>(TEntity entity);
         Task<int> Update<TEntity>(TEntity entity);
     }
