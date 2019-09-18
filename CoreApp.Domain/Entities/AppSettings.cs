ed83e5bdc487290de32a9f05393015ea124de579
Aderbal Farias <aderbalfarias@hotmail.com>
2019-09-18 23:54:27 +0100

    Adding comment

--- CoreApp.Domain/Entities/AppSettings.cs
+++ CoreApp.Domain/Entities/AppSettings.cs
@@ -1,5 +1,6 @@
 ﻿namespace CoreApp.Domain.Entities
 {
+    // Class to load objects in the appsettings json when the program starts
     public class AppSettings
     {
         public string ResponseErrorMessage { get; set; }
