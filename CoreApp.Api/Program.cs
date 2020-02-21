3303a3e09aade57b9324877f0ed1ff2315de7f70
Aderbal Farias <aderbalfarias@hotmail.com>
2020-02-21 15:31:23 +0000

    Adjusting Program cs

--- CoreApp.Api/Program.cs
+++ CoreApp.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -9,18 +10,21 @@ namespace CoreApp.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
             .ConfigureLogging(logging =>
             {
                 logging.ClearProviders();
                 logging.AddConsole();
                 logging.AddDebug();
                 logging.AddEventSourceLogger();
+            })
+            .ConfigureWebHostDefaults(webBuilder => 
+            {
+                webBuilder.UseStartup<Startup>();
             })
             .UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
     }
