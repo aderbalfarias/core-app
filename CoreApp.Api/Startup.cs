fd4a76efefc8ada9995002b514114da4cdca8e9a
Aderbal Farias <aderbalfarias@hotmail.com>
2020-02-22 17:08:04 +0000

    Adding reference

--- CoreApp.Api/Startup.cs
+++ CoreApp.Api/Startup.cs
@@ -6,6 +6,7 @@ using CoreApp.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
