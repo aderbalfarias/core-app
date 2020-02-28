9bf5a020ab7fd1952070e44907d0ed80996d30f1
Aderbal Farias <aderbalfarias@hotmail.com>
2020-02-28 13:20:32 +0000

    Adding unit test in order to check exception

--- CoreApp.UnitTest/Domain/SampleTest.cs
+++ CoreApp.UnitTest/Domain/SampleTest.cs
@@ -129,10 +129,22 @@ namespace CoreApp.UnitTest.Domain
             await RepositorySetup();
             var entity = MockSampleEntity.First();
 
-            await Assert.ThrowsAsync<NotImplementedException>(async () 
+            await Assert.ThrowsAsync<NotImplementedException>(async ()
                 => await _sampleServcice.Save(entity));
         }
+        
+        [Fact]
+        public async Task Save_Should_Throw_And_Check_Exception()
+        {
+            await RepositorySetup();
+            var entity = MockSampleEntity.First();
 
+            var exception = await Assert
+                .ThrowsAsync<NotImplementedException>(async ()
+                    => await _sampleServcice.Save(entity));
+
+            Assert.NotNull(exception);
+        }
 
         #endregion End Tests
     }
