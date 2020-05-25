b935780dfaf147c392f3cee6e15283487f071a14
Aderbal Farias <aderbalfarias@hotmail.com>
2020-05-25 23:16:16 +0100

    Adjusting code

--- CoreApp.Domain/CSharp8/IndicesAndRanges.cs
+++ CoreApp.Domain/CSharp8/IndicesAndRanges.cs
@@ -6,17 +6,18 @@
         {
             var words = new string[]
             {
-                                    // index from start    index from end
-                        "The",      // 0                   ^9
-                        "quick",    // 1                   ^8
-                        "brown",    // 2                   ^7
-                        "fox",      // 3                   ^6
-                        "jumped",   // 4                   ^5
-                        "over",     // 5                   ^4
-                        "the",      // 6                   ^3
-                        "lazy",     // 7                   ^2
-                        "dog"       // 8                   ^1
-            };                      // 9 (or words.Length) ^0
+                            // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+                            // 9 (or words.Length) ^0
+            };
 
             var quickBrownFox = words[1..4]; // "quick", "brown", and "fox"
             var lazyDog = words[^2..^0]; // "lazy" and "dog"
